afe87fa097dd5b51320a27043f1df29c1365b7d1
grachellecruz <grachelle.a.cruz@accenture.com>
2022-07-15 17:45:35 +0800

    peppa table

--- AdventureWorks2014/peppa.sql
+++ AdventureWorks2014/peppa.sql
@@ -3,4 +3,12 @@
 	[idnum] INT NOT NULL PRIMARY KEY , 
     [name] VARCHAR(50) NOT NULL, 
     [location] VARCHAR(50) NOT NULL
-)
+);
+GO
+INSERT INTO dbo.peppa ([idnum], [name], [location])
+VALUES 
+(1, 'Nico', 'Rizal'),
+(2, 'Jam', 'Au'),
+(3, 'Rutie', 'Batangas'),
+(4, 'Gra', 'Pasig');
+GO
